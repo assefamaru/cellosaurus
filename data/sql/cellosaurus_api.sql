c8ba958ba9ecfdb01d9d94008ffe400fbea3e9c4
assefamaru <me@alexandermaru.com>
2017-12-01 04:05:43 -0500

    First handler finished for listing cell line recordds. Created error handling module and json object rendering methods. Updated script for building db to include new table in database for maintaining stat data such as number of cell lines in current version, as well as version info. Updated routes.

--- data/sql/cellosaurus_api.sql
+++ data/sql/cellosaurus_api.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:45f24a3c8056d52b07774e82de5d5d326c669827a9f4d31c57db2cccb8c195f9
-size 61849429
+oid sha256:cba92600bd4e1c4249832761c0aac0178ca4e034abdb900aa770cb8581d3a4af
+size 61850236
