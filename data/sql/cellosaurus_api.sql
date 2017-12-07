c7b0219994d991946e8626ade0b7b99341142d87
assefamaru <me@alexandermaru.com>
2017-12-07 10:40:51 -0500

    Fix refs db schema, add references path, handler and helpers

--- data/sql/cellosaurus_api.sql
+++ data/sql/cellosaurus_api.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ee051912db2fab884289b2100f40557e906d0ec34fad512afecf4057593b2eaf
-size 6866974
+oid sha256:882f45c0da4141d97b1ff8df31de02a5c4dcb91f55222fde6c4bc1a9634fbb02
+size 10980297
