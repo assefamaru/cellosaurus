a7cd0e29d4f95117d410885450943b6423656aab
assefamaru <me@alexandermaru.com>
2017-12-07 11:06:12 -0500

    Fix convert sion script for attributes, finish db scschema

--- data/sql/cellosaurus_api.sql
+++ data/sql/cellosaurus_api.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:882f45c0da4141d97b1ff8df31de02a5c4dcb91f55222fde6c4bc1a9634fbb02
-size 10980297
+oid sha256:e071e80a747d4613c42f4b2a4fe47bbdaf6ce41d607b475d7141d67b1f936445
+size 53665679
